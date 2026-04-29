71681f07a83a509855efb2281c56b3399f35303f
shivam karoria <sk9965160@gmail.com>
2026-04-29 13:01:08 +0530

    firbebase added

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,4 +1,5 @@
 import { Outlet, Link, createRootRoute } from "@tanstack/react-router";
+import { AnalyticsTracker } from "@/components/AnalyticsTracker";
 
 function NotFoundComponent() {
   return (
@@ -28,5 +29,10 @@ export const Route = createRootRoute({
 });
 
 function RootComponent() {
-  return <Outlet />;
+  return (
+    <>
+      <AnalyticsTracker />
+      <Outlet />
+    </>
+  );
 }
